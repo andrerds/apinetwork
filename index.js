5361a273f130b40a6c3363c2bab0a7baa2c0cdd3
André Rds <andrerds.souza@gmail.com>
2020-07-09 22:38:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const domainPing = require("domain-ping");
+const admin = require('firebase-admin');
+const { response } = require('express');
+const app = require("express")();
+
+let serviceAccount = require('./rds-informatica-dff32377287a.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+app.get("/dominios", function(request, response){
+    db.collection('dominios').get().then((res ) => {
+        let data = [];
+        res.forEach((doc) => {
+            data.push({
+                id: doc.id,
+                dominio: doc.data().dominio
+            });
+            let result = [];
+            data.forEach(element => {
+               domainPing(element.dominio).then((res) => {
+                 return result.push(res)
+               }).catch(e => console.log(e)) ;
+              
+            });
+        })
+        response.json(result);
+     }).catch(err => { console.log(err)});
+})
+ 
+// 
+// dominios = ['posts.vix.br', 'google.com.br', 'facebook.com'];
+
+// console.log(dominios);
+
+// // Fazendo um loop para cada Interação 
+// dominios.forEach(element => {
+//     const check = domainPing(element).then((res) => {
+//         console.log(res)
+//     }).catch(e => console.log(e));
+// return check;
+// });
+// return dominios;
+ 
